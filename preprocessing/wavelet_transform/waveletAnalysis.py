2dc4f421fddb07c484b40f776290eaadaa25298b
Amey Noolkar <amey-anil.noolkar@warwick.ac.uk>
2023-07-27 19:26:42 +0100

    [DEV CHK] COI extracted.

--- preprocessing/wavelet_transform/waveletAnalysis.py
+++ preprocessing/wavelet_transform/waveletAnalysis.py
@@ -43,67 +43,87 @@ global_signif = wave_signif(variance, dt=DT, scale=scale, sigtest=1,
     lag1=LAG1, dof=dof, mother=MOTHER)
 
 #PLOTTING
-
-# --- Plot time series
 fig = plt.figure(figsize=(9, 10))
 gs = GridSpec(3, 4, hspace=0.4, wspace=0.75)
 plt.subplots_adjust(left=0.1, bottom=0.05, right=0.9, top=0.95,
                     wspace=0, hspace=0)
+
+coi_power = np.copy(power)
+for t_step, cap in enumerate(coi):
+    coi_power[np.argwhere(period > cap), t_step] = 0
+    
+print(f"Max power levels: {np.max(coi_power)}")
+
+# Plot the signal 
+# Contour plot of wavelet power spectrum
+pltx = plt.subplot(gs[0, 0:3])
+levels = [0, 0.5, 1, 2, 4, 999]
+# *** or use 'contour'
+CS = plt.contourf(time, period, coi_power, len(levels))
+im = plt.contourf(CS, levels=levels,
+    colors=['white', 'bisque', 'orange', 'orangered', 'darkred'])
+plt.xlabel('Time (seconds)')
+plt.ylabel('Frequency (seconds)')
+plt.title('c) Wavelet Power Spectrum')
+plt.xlim(xlim[:])
+# 95# significance contour, levels at -99 (fake) and 1 (95# signif)
+# plt.contour(time, period, sig95, [-99, 1], colors='k')
+# cone-of-influence, anything "above" is dubious
+plt.fill_between(time, coi * 0 + period[-1], coi, facecolor="none",
+    edgecolor="#00000040", hatch='x')
+plt.plot(time, coi, 'k')
+# format y-scale
+pltx.set_yscale('log')
+plt.ylim([np.min(period), np.max(period)])
+ax = plt.gca().yaxis
+ax.set_major_formatter(ticker.ScalarFormatter())
+pltx.ticklabel_format(axis='y', style='plain')
+
+# Plot the detrended signal
 plt.subplot(gs[1, 0:3])
 plt.plot(time, detrended_signal, 'k')
 plt.xlim(xlim[:])
-plt.xlabel('Time (year)')
-plt.ylabel('NINO3 SST (\u00B0C)')
-plt.title('a) NINO3 Sea Surface Temperature (seasonal)')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Flux')
+plt.title('b) Detrended flare')
 
-plt.text(time[-1] + 35, 0.5, 'Wavelet Analysis\nC. Torrence & G.P. Compo\n'
-    'http://paos.colorado.edu/\nresearch/wavelets/',
-    horizontalalignment='center', verticalalignment='center')
 
-# --- Contour plot wavelet power spectrum
+# Contour plot of wavelet power spectrum
 plt3 = plt.subplot(gs[2, 0:3])
 levels = [0, 0.5, 1, 2, 4, 999]
 # *** or use 'contour'
 CS = plt.contourf(time, period, power, len(levels))
 im = plt.contourf(CS, levels=levels,
     colors=['white', 'bisque', 'orange', 'orangered', 'darkred'])
-plt.xlabel('Time (year)')
-plt.ylabel('Period (years)')
-plt.title('b) Wavelet Power Spectrum (contours at 0.5,1,2,4\u00B0C$^2$)')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Frequency (seconds)')
+plt.title('c) Wavelet Power Spectrum')
 plt.xlim(xlim[:])
 # 95# significance contour, levels at -99 (fake) and 1 (95# signif)
 plt.contour(time, period, sig95, [-99, 1], colors='k')
-# cone-of-influence, anything "below" is dubious
+# cone-of-influence, anything "above" is dubious
 plt.fill_between(time, coi * 0 + period[-1], coi, facecolor="none",
     edgecolor="#00000040", hatch='x')
 plt.plot(time, coi, 'k')
 # format y-scale
-plt3.set_yscale('log')#, base=10, subs=None)
+plt3.set_yscale('log')
 plt.ylim([np.min(period), np.max(period)])
 ax = plt.gca().yaxis
 ax.set_major_formatter(ticker.ScalarFormatter())
 plt3.ticklabel_format(axis='y', style='plain')
 
-# set up the size and location of the colorbar
-# position=fig.add_axes([0.5,0.36,0.2,0.01])
-# plt.colorbar(im, cax=position, orientation='horizontal')
-#   , fraction=0.05, pad=0.5)
-
-# plt.subplots_adjust(right=0.7, top=0.9)
-
 # --- Plot global wavelet spectrum
-plt4 = plt.subplot(gs[1, -1])
+plt4 = plt.subplot(gs[2, -1])
 plt.plot(global_ws, period)
 plt.plot(global_signif, period, '--')
-plt.xlabel('Power (\u00B0C$^2$)')
-plt.title('c) Global Wavelet Spectrum')
+plt.xlabel('Power')
+plt.title('d) Global Wavelet Spectrum')
 plt.xlim([0, 1.25 * np.max(global_ws)])
 # format y-scale
-plt4.set_yscale('log')#, base=2, subs=None)
+plt4.set_yscale('log')
 plt.ylim([np.min(period), np.max(period)])
 ax = plt.gca().yaxis
 ax.set_major_formatter(ticker.ScalarFormatter())
 plt4.ticklabel_format(axis='y', style='plain')
-
 
 plt.show()
